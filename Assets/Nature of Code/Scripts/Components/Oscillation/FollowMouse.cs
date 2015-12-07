68936e4af4b0763aa877bf92dfec06ea59831176
Karunia Ramadhan <hitnoodle@gmail.com>
2015-12-07 18:26:46 +0700

    moving folder around, trigonometry

---
+++ Assets/Nature of Code/Scripts/Components/Oscillation/FollowMouse.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour
+{
+    public Vector2 Location;
+    public Vector2 Velocity;
+    public Vector2 Acceleration;
+
+    protected Transform _Transform;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _Transform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = mousePos - new Vector2(_Transform.position.x, _Transform.position.y);
+        dir.Normalize();
+        dir *= 0.5f;
+        Acceleration = dir;
+
+        Velocity += Acceleration * Time.deltaTime;
+        Location += Velocity * Time.deltaTime;
+
+        Acceleration *= 0;
+
+        float angle = Mathf.Atan2(Velocity.x, Velocity.y);
+
+        _Transform.Rotate(new Vector3(0, 0, angle));
+        _Transform.position = Location;
+    }
+}
---
+++ Assets/Nature of Code/Scripts/Components/Oscillation/RotatePolar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePolar : MonoBehaviour
+{
+    public float Theta = 0f;
+    public float Speed = Mathf.PI;
+
+    protected Transform _Transform;
+    protected float _Radius;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _Transform = transform;
+        _Radius = (_Transform.position).magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = _Radius * Mathf.Sin(Theta);
+        float y = _Radius * Mathf.Cos(Theta);
+
+        _Transform.position = new Vector2(x, y);
+        Theta += Speed * Time.deltaTime;
+	}
+}
